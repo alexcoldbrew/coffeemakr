ff8c7414436859f7138f1b7a3b478d416d1e4eca
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-26 19:09:42 -0600

    resource generator for Review, and added user_id to Recipe

--- db/migrate/20210127010524_create_recipes.rb
+++ db/migrate/20210127010524_create_recipes.rb
@@ -4,6 +4,7 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :description
       t.datetime :date_created
+      t.integer :user_id
 
       t.timestamps
     end
