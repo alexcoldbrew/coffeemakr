226fb63bd66dcf19936cc9bb517fcc29b513e0ec
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-27 21:01:36 -0600

    created super basic show page and updated show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     end
 
     def show
+        @user = User.find_by_id(params[:id])
     end
 
     def new
@@ -11,6 +12,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def edit
@@ -20,6 +28,10 @@ class UsersController < ApplicationController
     end
 
     def destroy
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
