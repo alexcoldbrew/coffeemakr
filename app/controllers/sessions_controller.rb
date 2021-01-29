d56e42d3c766acc31e995c68757f57cd904c12ce
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-29 10:17:01 -0600

    working on login/signup flow, added AppController methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
     end
 
     def new
-
     end
 
     def create
-
+        u = User.find_by_email(params[:email])
+        if u && u.authenticate(params[:password])
+            session[:user_id] = u.id
+            redirect_to user_path(u)
+        else
+            flash[:alert] = "Invalid credentials. Please try again!"
+            redirect_to '/login'
+        end
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,11 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
+        @user = User.create(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
+            # maybe redirect to welcome page after sign up?
         else
             render :new
         end
