d2901bbc41e0ef29cd00c56175be900dec211b36
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-31 15:24:49 -0600

    working on new recipe form

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,33 @@
 class IngredientsController < ApplicationController
+
+    def index
+        @ingredients = Ingredient.all
+    end
+
+    def show
+    end
+
+    def new
+        @ingredient = Ingredient.new
+    end
+
+    def create
+        @ingredient = Ingredient.create(ingredient_params)
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def ingredients_params
+        params.require(:ingredient).permit(:name, :amount)
+    end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
     has_and_belongs_to_many :ingredients
+
 end
