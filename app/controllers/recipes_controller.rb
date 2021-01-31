275bac89c5454d30f7038795829572e6b94e1df1
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-31 09:56:34 -0600

    adding CRUD actions and working on New Recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,35 @@ class RecipesController < ApplicationController
         @recipes = Recipe.all
     end
 
+    def show
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.create(recipe_params)
+        if @recipe.save
+            redirect_to recipe_path(@recipe)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :date_created, :user_id)
+    end
+
 end
