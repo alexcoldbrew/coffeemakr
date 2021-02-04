8ad88e9874b108064064523b41d5dacd47210f3f
alexcoldbrew <alexcoldbrew@gmail.com>
2021-02-04 09:37:57 -0600

    added class level scopes to ingredient and recipe to replace prev query method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ApplicationRecord
     has_and_belongs_to_many :recipes
     validates :name, presence: true
     validates :name, uniqueness: true
+
+    scope :alphabetize, -> { order(:name) }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,6 @@ class Recipe < ApplicationRecord
     has_many :users, through: :reviews
     has_and_belongs_to_many :ingredients
     validates :name, :description, :date_created, presence: true
+
+    scope :alphabetize, -> { order(:name) }
 end
