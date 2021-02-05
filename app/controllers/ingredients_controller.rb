4acd92d5dd8fa8d81a285a4cf96cefb4134b9516
alexcoldbrew <alexcoldbrew@gmail.com>
2021-02-05 08:13:37 -0600

    added to ingreients CRUD actions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -27,13 +27,25 @@ class IngredientsController < ApplicationController
         end
     end
 
+    # can change this to only let an admin edit/update/delete ingredients if wanted..
+
     def edit
+        @ingredient = Ingredient.find_by_id(params[:id])
     end
 
     def update
+        @ingredient = Ingredient.find_by_id(params[:id])
+        if @ingredient.update(ingredients_params)
+            redirect_to ingredient_path(@ingredient)
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @ingredient = Ingredient.find_by_id(params[:id])
+        @ingredient.destroy
+        redirect_to ingredients_path
     end
 
     private
