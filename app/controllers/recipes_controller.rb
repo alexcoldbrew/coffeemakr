678e96f4b3a7f476e81bbebc819105a349b933eb
alexcoldbrew <alexcoldbrew@gmail.com>
2021-02-07 15:32:52 -0600

    added before_action to RecipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,11 @@
 class RecipesController < ApplicationController
+    before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
     def index
         @recipes = Recipe.all
     end
 
     def show
-        @recipe = Recipe.find_by_id(params[:id])
         @reviews = @recipe.reviews
     end
 
@@ -26,14 +26,12 @@ class RecipesController < ApplicationController
     end
 
     def edit
-        @recipe = Recipe.find_by_id(params[:id])
         unless current_user.id == @recipe.user_id
             redirect_to recipe_path(@recipe)
         end
     end
 
     def update
-        @recipe = Recipe.find_by_id(params[:id])
         if current_user.id == @recipe.user_id
             if @recipe.update(recipe_params)
                 redirect_to recipe_path(@recipe)
@@ -46,7 +44,6 @@ class RecipesController < ApplicationController
     end
 
     def destroy
-        @recipe = Recipe.find_by_id(params[:id])
         @recipe.destroy
         redirect_to recipes_path
     end
@@ -57,4 +54,7 @@ class RecipesController < ApplicationController
         params.require(:recipe).permit(:name, :description, :date_created, :user_id, :ingredient_ids, ingredient_ids: [])
     end
 
+    def find_recipe
+        @recipe = Recipe.find_by_id(params[:id])
+    end
 end
